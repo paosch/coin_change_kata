df1bc596a19705fdbeeac9073d33afbee90f078d
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-11 16:41:14 +0100

    Refactored: turned array of integers into hash of currency

--- lib/code_changer.rb
+++ lib/code_changer.rb
@@ -1,16 +1,17 @@
 class Codechanger
-  attr_reader :coin_array
+  attr_reader :coin_hash
   def initialize
-    @coin_array = [200, 100, 50, 20, 10, 5, 2, 1]
+    @coin_hash = { 200 => '£2', 100 => '£1', 50 => '50p', 20 => '20p',
+                   10 => '10p', 5 => '5p', 2 => '2p', 1 => '1p' }
   end
 
   def change(amount)
     raise 'Not an integer' unless amount.is_a? Integer
     coin_holder = []
-    @coin_array.each do |coin|
-      (amount / coin).times do
-        coin_holder << coin
-        amount -= coin
+    @coin_hash.each do |k, v|
+      (amount / k).times do
+        coin_holder << v
+        amount -= k
       end
     end
     coin_holder
--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -1,7 +1,7 @@
 require 'code_changer'
 describe Codechanger do
-  it 'contains and array of coins' do
-    expect(subject.coin_array).to eq [200, 100, 50, 20, 10, 5, 2, 1]
+  it 'contains a hash of coins' do
+    expect(subject.coin_hash).to eq({200 => '£2', 100 => '£1', 50 => '50p', 20 => '20p', 10 => '10p', 5 => '5p', 2 => '2p', 1 => '1p' })
   end
 
   describe '.change()' do
@@ -9,12 +9,12 @@ describe Codechanger do
       expect { subject.change('thrirty pounds') }.to raise_error('Not an integer')
     end
 
-    it 'accepts 1000p and returns an array of 5 2 pound coins' do
-      expect(subject.change(1000)).to eq [200, 200, 200, 200, 200]
+    it 'accepts 1000p and returns an array of 5 £2 coins' do
+      expect(subject.change(1000)).to eq ['£2', '£2', '£2', '£2', '£2']
     end
 
-    it 'accepts 2500p and returns an array of 5 2 pound coins' do
-      expect(subject.change(2500)).to eq [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 100]
+    it 'accepts 2500p and returns an array of 12 £2 coins and one £1 coin' do
+      expect(subject.change(2500)).to eq ['£2', '£2', '£2', '£2', '£2', '£2', '£2', '£2', '£2', '£2', '£2', '£2', '£1']
     end
   end
 end
