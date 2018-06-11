2ceea7a5b6297dec2d27d4bcbbd08dbf320751f4
Alfie Darko <me@alfiedarko.co.uk>
2018-06-11 14:12:55 +0100

    GREEN: Codechanger > .change() > only accepts integers as argmuments

--- lib/code_changer.rb
+++ lib/code_changer.rb
@@ -3,4 +3,8 @@ class Codechanger
   def initialize
     @coin_array = [200, 100, 50, 20, 10, 5, 2, 1]
   end
+
+  def change(amount)
+    raise 'Not an integer' unless amount.is_a? Integer
+  end
 end
--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -3,4 +3,10 @@ describe Codechanger do
   it 'contains and array of coins' do
     expect(subject.coin_array).to eq [200, 100, 50, 20, 10, 5, 2, 1]
   end
+
+  describe '.change()' do
+    it 'only accepts integers as an argument' do
+      expect { subject.change('thrirty pounds') }.to raise_error('Not an integer')
+    end
+  end
 end
